2b3c00a5c1ce45b4e80725539b7be728d4a824b9
Victor Perin <me@victorperin.com>
2015-08-14 14:26:02 -0300

    removendo variáveis a mais no coleção Posts do mongo

--- server/fixtures.js
+++ server/fixtures.js
@@ -4,15 +4,13 @@ if (Posts.find().count() === 0) {
   Posts.insert({
     titulo: 'First Post',
     data: new Date('2014-01-22T14:56:59.301Z'),
-    conteudo: '<p>teste</p><br/><code>qwertyuiop</code>',
-    flagged: false
+    conteudo: '<p>teste</p><br/><code>qwertyuiop</code>'
   });
 
   Posts.insert({
     titulo: 'Second Post',
     data: new Date('2014-01-23T14:56:59.301Z'),
-    conteudo: '<p>teste</p><br/><b>qwertyuiop</b>',
-    flagged: false
+    conteudo: '<p>teste</p><br/><b>qwertyuiop</b>'
   });
 
   Categorias.insert({
