7a5a272df50cfc8c89064d31216d8307f8f76b2e
Victor Perin <me@victorperin.com>
2015-08-14 14:28:18 -0300

    alterando posição das categorias, para poder inseri-las nos posts

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,6 +1,13 @@
 if (Posts.find().count() === 0) {
   console.log("Populando banco de dados...");
 
+  Categorias.insert({
+    nome: "humildade"
+  });
+  Categorias.insert({
+    nome: "blog"
+  });
+
   Posts.insert({
     titulo: 'First Post',
     data: new Date('2014-01-22T14:56:59.301Z'),
@@ -11,13 +18,6 @@ if (Posts.find().count() === 0) {
     titulo: 'Second Post',
     data: new Date('2014-01-23T14:56:59.301Z'),
     conteudo: '<p>teste</p><br/><b>qwertyuiop</b>'
-  });
-
-  Categorias.insert({
-    nome: "humildade"
-  });
-  Categorias.insert({
-    nome: "blog"
   });
 }else{
   console.log("Banco de dados já está populado, por favor, execute 'meteor reset' no terminal.")
