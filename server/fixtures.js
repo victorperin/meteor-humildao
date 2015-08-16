fc518f4a8a0856300f56e389265bbd62fbeffdaf
Victor Perin <me@victorperin.com>
2015-08-16 01:04:42 -0300

    inserindo categorias nos posts

--- server/fixtures.js
+++ server/fixtures.js
@@ -11,13 +11,15 @@ if (Posts.find().count() === 0) {
   Posts.insert({
     titulo: 'First Post',
     data: new Date('2014-01-22T14:56:59.301Z'),
-    conteudo: '<p>teste</p><br/><code>qwertyuiop</code>'
+    conteudo: '<p>teste</p><br/><code>qwertyuiop</code>',
+    categorias: Categorias.find({nome:'humildade'}, {_id:1}).map(function(item){ return item._id; })
   });
 
   Posts.insert({
     titulo: 'Second Post',
     data: new Date('2014-01-23T14:56:59.301Z'),
-    conteudo: '<p>teste</p><br/><b>qwertyuiop</b>'
+    conteudo: '<p>teste</p><br/><b>qwertyuiop</b>',
+    categorias: Categorias.find({nome:'blog'}, {_id:1}).map(function(item){ return item._id; })
   });
 }else{
   console.log("Banco de dados já está populado, por favor, execute 'meteor reset' no terminal.")
