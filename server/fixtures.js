6c61dc1525298a9d0c1c4e056ac52d0e60715012
Victor Perin <me@victorperin.com>
2015-08-30 17:09:17 -0300

    inserindo o valor do ID das categorias, no post

--- server/fixtures.js
+++ server/fixtures.js
@@ -12,14 +12,14 @@ if (Posts.find().count() === 0) {
     titulo: 'First Post',
     data: new Date('2014-01-22T14:56:59.301Z'),
     conteudo: '<p>teste</p><br/><code>qwertyuiop</code>',
-    categorias: Categorias.find({nome:'humildade'}, {_id:1}).map(function(item){ return item._id; })
+    categorias: Categorias.find({nome:'humildade'}, {_id:1}).map(function(item){ return item._id.valueOf(); })
   });
 
   Posts.insert({
     titulo: 'Second Post',
     data: new Date('2014-01-23T14:56:59.301Z'),
     conteudo: '<p>teste</p><br/><b>qwertyuiop</b>',
-    categorias: Categorias.find({nome:'blog'}, {_id:1}).map(function(item){ return item._id; })
+    categorias: Categorias.find({nome:'blog'}, {_id:1}).map(function(item){ return item._id.valueOf(); })
   });
 }else{
   console.log("Banco de dados já está populado, por favor, execute 'meteor reset' no terminal.")
